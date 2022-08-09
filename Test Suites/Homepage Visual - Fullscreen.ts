0b13658b8e311ea6a1de53960c83ed5f3e57c2d0
adamtopczewki-bandit <adam.topczewski@bandit.house>
2022-08-09 10:28:55 +0200

    added missing test suite for vt

---
+++ Test Suites/Homepage Visual - Fullscreen.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Homepage Visual - Fullscreen</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>62bd424b-990d-4482-b60e-a49669dfd37b</testSuiteGuid>
+   <testCaseLink>
+      <guid>50f9cb22-4f1a-473b-9c36-cfcf72c65d07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Desktop Tests/Visual Testing/Homepage FullScreen Visual Testing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
